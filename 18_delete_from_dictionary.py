c6a543d5989bafd99485f20f3299f95b9be54410
Robert Vari <mail.robertvari@gmail.com>
2025-05-10 17:42:58 +0200

    update

---
+++ 18_delete_from_dictionary.py
@@ -0,0 +1,9 @@
+phonebook = {
+    "06 20 123 4567": {"name": "Kiss Csaba", "address": "Budapest"}, 
+    "06 30 987 6543": {"name": "Kocsis Krisztina", "address": "Pécs"}, 
+    "06 20 756 9382": {"name": "Nagy Balázs", "address": "Debrecen"},
+    "06 20 758 9684": {"name": "Németh Attila", "address": "Miskolc"}
+}
+
+del phonebook["06 20 123 4567"]
+print(phonebook)
