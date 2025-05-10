9951bfd1a0596e13b7969ab882041ebc78135fb0
Robert Vari <mail.robertvari@gmail.com>
2025-05-10 16:37:20 +0200

    update

---
+++ 10_extend_list.py
@@ -0,0 +1,10 @@
+my_friends = ["Csaba", "Csilla"]
+my_family = ["Tamás", "Balázs", "János"]
+
+# extend first list with other
+my_friends.extend(my_family)
+
+# add two lists together
+friends_and_family = my_friends + my_family
+
+print(friends_and_family)
