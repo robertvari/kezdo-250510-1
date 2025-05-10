af133d17607221d04e37c087852c95bbbb6b2a6b
Robert Vari <mail.robertvari@gmail.com>
2025-05-10 15:38:53 +0200

    update

---
+++ 05_formated_string.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+address = input("Where do you live? ")
+email = input("What is your emaild address? ")
+
+# f = formated string
+print(f"Your name is {name}. You live in {address}. Your email is: {email}")
