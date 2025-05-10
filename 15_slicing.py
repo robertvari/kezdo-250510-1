73f3eb452c863b3a8e9f7c6f4eb6dce4ce90bafc
Robert Vari <mail.robertvari@gmail.com>
2025-05-10 17:09:03 +0200

    update

---
+++ 15_slicing.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+name = "Robert Vari"
+
+# list[START:STOP:STEP]
+
+print(numbers[2:5])
+print(numbers[4:])
+print(numbers[:-2])
+print(name[2:])
+print(numbers[::3])
+print(numbers[::-1])
+print(name[::-1])
