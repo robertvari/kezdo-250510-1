6ad61af72d21f3a5fa91c171cf4794b5cf475e76
Robert Vari <mail.robertvari@gmail.com>
2025-05-10 16:27:12 +0200

    update

--- 07_lists.py
+++ 07_lists.py
@@ -2,4 +2,4 @@
 # index          0         1         2         3         4
 my_friends = ["Csilla", "Csaba", "Kriszta", "Tamás", "Balázs"]
 
-print(  my_friends[0]  )
+print(  my_friends[-1]  )
---
+++ 08_add_item_to_list.py
@@ -0,0 +1,13 @@
+my_friends = []
+
+name = input("Give me a name: ")
+my_friends.append(name)
+print(my_friends)
+
+name = input("Give me a name: ")
+my_friends.append(name)
+print(my_friends)
+
+name = input("Give me a name: ")
+my_friends.append(name)
+print(my_friends)
