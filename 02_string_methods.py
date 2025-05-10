47e7499de4deca9af1338714d15a0810e9d3332d
Robert Vari <mail.robertvari@gmail.com>
2025-05-10 14:57:55 +0200

    update

---
+++ 02_string_methods.py
@@ -0,0 +1,7 @@
+print(  "Hello World".lower()  )
+print(  "Hello World".upper()  )
+print(  "kiss csaba".title()  )
+print(  "Hello World".replace("World", "Csaba")  )
+
+# chaning methods
+print( "kiss csaba".title().replace("Kiss", "Nagy") )
