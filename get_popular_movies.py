654e5984eca2de14f832d4342bc5cf3fecf53595
Robert Vari <mail.robertvari@gmail.com>
2025-05-11 17:04:07 +0200

    update

--- get_popular_movies.py
+++ get_popular_movies.py
@@ -3,4 +3,16 @@ import pprint
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
 
 movies = tmdb.Movies()
-pprint.pprint(movies.popular())
+
+for page in range(1, 11):
+    print("-"*50, f"Current page: {page}", "-"*50)
+    # get popular movies for current page
+    popular_movies = movies.popular(page=page)
+    
+    # get movie list from the dictionary
+    movie_list = popular_movies["results"]
+
+    # step through the movie list
+    for movie in movie_list:
+        # because movie is a dictionary we can get the title
+        print(movie["title"])
