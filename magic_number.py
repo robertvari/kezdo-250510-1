186ecb3fdf24d4fd82feaa5372634aa81c2a194c
Robert Vari <mail.robertvari@gmail.com>
2025-05-11 16:35:23 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -1,5 +1,11 @@
+import os, random
+
 # Game Loop
 while True:
+    os.system("cls")
+    # mac/linux
+    # os.system("clear")
+
     # Game intro
     print("-"*50, "MAGIC NUMBER", "-"*50)
     print("I have number between 1 and 10. Can you guess it?")
@@ -11,17 +17,26 @@ while True:
     tries = 3
 
     # computer guess
-    magic_number = str( 5 )
+    magic_number = str( random.randint(min_number, max_number) )
 
     # get player guess
     player_guess = input("Your number: ")
 
+    # force player to try again if the guess is wrong
     while magic_number != player_guess:
         tries -=1
         if tries == 0: break
         
+        os.system("cls")
         print(f"Wrong guess. You have {tries} tries left. Try again.")
         player_guess = input("Your number: ")
+    
+    os.system("cls")
+    # game end condition
+    if magic_number == player_guess:
+        print("You win! :))")
+    else:
+        print("You lost this round")
 
     player_response = input("Do you want to play again? (y/n)")
     if player_response == "n": break
---
+++ test.py
@@ -0,0 +1,2 @@
+import tmdbsimple
+print(tmdbsimple)
